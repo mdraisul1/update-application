c6d6660f39d68196425a91672f091be7688e309d
Raisul Islam <mdraju3248@gmail.com>
2024-09-30 01:50:08 +0600

    Add database connection

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php 
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "password";
+$db_name = "demo_two";
+
+// database connection query
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if(!$conn){
+    die("Database connection failed");
+}else{
+    echo "Database connection successful";
+}
+
+
